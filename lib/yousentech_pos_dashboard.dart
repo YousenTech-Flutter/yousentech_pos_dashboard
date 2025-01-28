600fba5b26cb7bd52068c53b6a32aad6b2206ea8
mohmmedsaleh <mohmmed123232@gmail.com>
2025-01-28 12:45:29 +0300

    .

--- lib/yousentech_pos_dashboard.dart
+++ lib/yousentech_pos_dashboard.dart
@@ -1,5 +1,6 @@
 /// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+// class Calculator {
+//   /// Returns [value] plus 1.
+//   int addOne(int value) => value + 1;
+// }
+library yousentech_pos_dashboard;
