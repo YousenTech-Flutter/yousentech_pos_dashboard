4b6a3493cfe16e00a7d160d342fc934c15084fd8
eemzain <hudazain55@gmail.com>
2025-12-22 12:49:20 +0300

    .

--- lib/dashboard/src/presentation/views/home.dart
+++ lib/dashboard/src/presentation/views/home.dart
@@ -30,6 +30,7 @@ class _HomeState extends State<Home> {
   int _navIndex = 0;
   @override
   Widget build(BuildContext context) {
+    print("Home=============");
     return 
         SafeArea(
           child: Scaffold(
