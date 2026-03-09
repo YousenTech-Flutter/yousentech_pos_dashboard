ebfb1496a6aa0fba5fdbc14367e36b6e530c1c57
Salehbafadhel <158081925+Salehbafadhel@users.noreply.github.com>
2026-03-09 12:01:00 +0300

    .

--- lib/dashboard/src/presentation/widgets/progress_bar.dart
+++ lib/dashboard/src/presentation/widgets/progress_bar.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shared_widgets/config/app_colors.dart';
 import 'package:shared_widgets/config/app_images.dart';
+import 'package:shared_widgets/config/app_sizes.dart';
 import 'package:shared_widgets/config/theme_controller.dart';
 import 'package:shared_widgets/utils/responsive_helpers/size_helper_extenstions.dart';
 import 'package:yousentech_pos_loading_synchronizing_data/loading_sync/src/domain/loading_item_count_controller.dart';
@@ -37,83 +38,95 @@ class _ProgressWidgetState extends State<ProgressWidget> {
       overlayChildBuilder: (context) {
         return Positioned(
           child: Obx(() {
-              return Material(
-                color: Get.find<ThemeController>().isDarkMode.value  ? AppColor. darkModeBackgroundColor : Color(0xFFDDDDDD),
-                child:  Center(
-                    child: SizedBox(
-                      width: context.setWidth(454.48),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image.asset(
-                            AppImages.logoGif,
-                            package: 'shared_widgets',
-                            fit: BoxFit.contain,
-                            width: context.setMinSize(50),
-                            height: context.setMinSize(50),
-                            filterQuality: FilterQuality.high,
-                          ),
-                          Padding(
-                            padding: EdgeInsets.all(context.setMinSize(8)),
-                            child: Directionality(
-                              textDirection: TextDirection.ltr,
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    "${loadingDataController.loadTital.value} ",
-                                    style: TextStyle(
-                                      color:Get.find<ThemeController>().isDarkMode.value 
-                                              ? AppColor.white : AppColor.black,
-                                      // fontSize: context.setSp(16),
-                                      fontSize: context.setSp(22),
-                                      fontWeight: FontWeight.w500,
-                                      height: 1.43,
-                                    ),
+            return Material(
+                color: Get.find<ThemeController>().isDarkMode.value
+                    ? AppColor.darkModeBackgroundColor
+                    : Color(0xFFDDDDDD),
+                child: Center(
+                  child: SizedBox(
+                    width: context.setWidth(454.48),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          AppImages.logoGif,
+                          package: 'shared_widgets',
+                          fit: BoxFit.contain,
+                          width: context.setMinSize(50),
+                          height: context.setMinSize(50),
+                          filterQuality: FilterQuality.high,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(context.setMinSize(8)),
+                          child: Directionality(
+                            textDirection: TextDirection.ltr,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "${loadingDataController.loadTital.value} ",
+                                  style: TextStyle(
+                                    color: Get.find<ThemeController>()
+                                            .isDarkMode
+                                            .value
+                                        ? AppColor.white
+                                        : AppColor.black,
+                                    fontSize: context.setSp(AppSizes.subTitle),
+                                    fontWeight: FontWeight.w500,
+                                    height: 1.43,
                                   ),
-                                  loadingDataController.isLoadData.value
-                                      ? Text(
+                                ),
+                                loadingDataController.isLoadData.value
+                                    ? Text(
                                         "...",
                                         style: TextStyle(
-                                          color:Get.find<ThemeController>().isDarkMode.value 
-                                              ? AppColor.white : AppColor.black,
-                                          
-                                          // fontSize: context.setSp(16),
-                                          fontSize: context.setSp(22),
+                                          color: Get.find<ThemeController>()
+                                                  .isDarkMode
+                                                  .value
+                                              ? AppColor.white
+                                              : AppColor.black,
+                                          fontSize:
+                                              context.setSp(AppSizes.subTitle),
                                           fontWeight: FontWeight.w500,
                                           height: 1.43,
                                         ),
                                       )
-                                      : Text(
+                                    : Text(
                                         "( ${_loadingItemsCountController.loadingItemCount.value.toString()} ",
                                         style: TextStyle(
-                                          color:Get.find<ThemeController>().isDarkMode.value 
-                                              ? AppColor.white : AppColor.black,
-                                          // fontSize: context.setSp(16),
-                                          fontSize: context.setSp(22),
+                                          color: Get.find<ThemeController>()
+                                                  .isDarkMode
+                                                  .value
+                                              ? AppColor.white
+                                              : AppColor.black,
+                                          fontSize:
+                                              context.setSp(AppSizes.subTitle),
                                           fontWeight: FontWeight.w500,
                                           height: 1.43,
                                         ),
                                       ),
-                                  loadingDataController.isLoadData.value
-                                      ? Container()
-                                      : Text(
+                                loadingDataController.isLoadData.value
+                                    ? Container()
+                                    : Text(
                                         "/ ${loadingDataController.lengthRemote.value} )",
                                         style: TextStyle(
-                                          color:Get.find<ThemeController>().isDarkMode.value 
-                                              ? AppColor.white : AppColor.black,
-                                          // fontSize: context.setSp(16),
-                                          fontSize: context.setSp(22),
+                                          color: Get.find<ThemeController>()
+                                                  .isDarkMode
+                                                  .value
+                                              ? AppColor.white
+                                              : AppColor.black,
+                                          fontSize:
+                                              context.setSp(AppSizes.subTitle),
                                           fontWeight: FontWeight.w500,
                                           height: 1.43,
                                         ),
                                       ),
-                                ],
-                              ),
+                              ],
                             ),
                           ),
-                          (loadingDataController.isLoad.value)
-                              ? SizedBox(
+                        ),
+                        (loadingDataController.isLoad.value)
+                            ? SizedBox(
                                 width: context.setWidth(454.48),
                                 child: LinearProgressIndicator(
                                   color: AppColor.appColor,
@@ -123,15 +136,12 @@ class _ProgressWidgetState extends State<ProgressWidget> {
                                   ),
                                 ),
                               )
-                              : Container(),
-                        ],
-                      ),
+                            : Container(),
+                      ],
                     ),
-                  )
-                
-              );
-            }
-          ),
+                  ),
+                ));
+          }),
         );
       },
     );
